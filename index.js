91641354026abc8c615219bc1f76e61f8b68fc52
Vasyl Okhrimenko <64694836+v-okhrimenko@users.noreply.github.com>
2023-10-07 17:50:48 +0200

    add - video loop mute true

--- index.js
+++ index.js
@@ -109,6 +109,8 @@ for (let i = 0; i < txtDescription.length; i++) {
     let video = document.createElement('video')
     video.className = "video"
     video.src = txtDescription[i].video;
+
+    // <video autoplay muted playsinline> <source src="your-video.mp4" type="video/mp4"> Your browser does not support the video tag. </video>
     video.autoplay = "true";
     video.muted = "true"
     video.loop = "true";
